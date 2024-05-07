94651eda9d39325a9b2a80106554e67a2d75cfc9
soman-maharjan <maharjansoman@yahoo.com>
2024-05-07 14:57:12 +0545

    fixed sharp issue

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
@@ -9,9 +9,6 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  image: {
-    service: passthroughImageService(),
-  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
